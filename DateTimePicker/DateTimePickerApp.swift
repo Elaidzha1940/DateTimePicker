a707cc84826c2810084862e3cf1b370156b4585f
Elaidzha1940 <eli080293@gmail.com>
2023-09-18 21:42:35 +0300

    DateTimePicker

---
+++ DateTimePicker/DateTimePickerApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: DateTimePicker
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 18.09.2023.
+//
+//  */
+
+import SwiftUI
+
+@main
+struct DateTimePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
